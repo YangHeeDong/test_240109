d76f1d5c8c614cccff7741707625c00e94f4dcf5
hdYang <gmlehd0201@gmail.com>
2024-01-09 14:10:15 +0900

    first commit

---
+++ src/main/java/com/example/demo_240109/Demo240109Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_240109;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo240109Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo240109Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_240109/Demo240109ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_240109;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo240109ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
